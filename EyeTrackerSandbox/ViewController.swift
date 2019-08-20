e5ca5fd85988036571dca1617911fe86f906b826
hori <yuki.hrgc@gmail.com>
2019-08-20 22:45:58 +0900

    Initial Commit

---
+++ EyeTrackerSandbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EyeTrackerSandbox
+//
+//  Created by 堀口祐樹 on 2019/08/20.
+//  Copyright © 2019 Yuki Horiguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
